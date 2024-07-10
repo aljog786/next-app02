7aa74f54a1657d54b14f2fdb309b9460b9ab034d
aljog786 <aljog786@gmail.com>
2024-07-10 14:49:01 +0530

    nav-links(active):using usePathName

--- news-app/components/main-header.js
+++ news-app/components/main-header.js
@@ -1,11 +1,7 @@
-"use client"
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-
+import NavLink from './nav-link';
 
 const MainHeader = () => {
-  const path = usePathname();
   return (
     <header id="main-header">
       <div id="logo">
@@ -14,14 +10,10 @@ const MainHeader = () => {
       <nav>
         <ul>
           <li>
-            <Link href="/news" className={path.startsWith('/news') ? 'active' : undefined}>
-            News
-            </Link>
+            <NavLink href="/news">News</NavLink>
           </li>
           <li>
-            <Link href="/archive" className={path.startsWith('/archive') ? 'active' : undefined}>
-            Archive
-            </Link>
+            <NavLink href="/archive">Archive</NavLink>
           </li>
         </ul>
       </nav>
